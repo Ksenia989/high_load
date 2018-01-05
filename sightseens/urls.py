7d1e301689b216310fa8a4d2bee735fed41063b2
Ksenia <torgaeva.ksenia@yandex.ru>
2018-01-05 21:23:16 +0300

    изменила вьюшки и юрл к ним на более правилные

--- sightseens/urls.py
+++ sightseens/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'^(\d+)/(\d+)$', views.getEntity, name='get_entity'),
-    path(r'^users/(\d+)/visits$', views.getPersonVisitList, name='get_visit_list_for_person'),
+    path('<str:entity>/<int:entity_id>', views.getEntity, name='get_entity'),
+    path('users/<int:entity_id>/visits', views.getPersonVisitList, name='get_visit_list_for_person'),
     path(r'^location/(\d+)/avg', views.getLocationAverageMark, name='get_average_mark_for_location'),
-    path(r'^(\d+)/(\d+)$', views.updateUser, name='update_user'),  # entity, id
+    # path(r'^(\d+)/(\d+)$', views.updateUser, name='update_user'),  # entity, id
     path(r'^(\d+)/new$', views.createUser, name='new_user'),  # user
 ]
