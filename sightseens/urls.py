8ec303a1c63f6b238118290e42e78afdb2fedfc1
Ksenia <torgaeva.ksenia@yandex.ru>
2018-01-05 09:13:34 +0300

    добавила методы к новым url

--- sightseens/urls.py
+++ sightseens/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # todo path(r'^post/(\d+)/$', views.getEntity, name='get_entity'),
+    path(r'^(\d+)/(\d+)$', views.getEntity, name='get_entity'),
+    path(r'^users/(\d+)/visits$', views.getPersonVisitList, name='get_visit_list_for_person'),
+    path(r'^location/(\d+)/avg', views.getLocationAverageMark, name='get_average_mark_for_location'),
+    path(r'^(\d+)/(\d+)$', views.updateUser, name='update_user'),  # entity, id
+    path(r'^(\d+)/new$', views.createUser, name='new_user'),  # user
 ]
