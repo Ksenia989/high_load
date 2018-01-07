5eabae8afe9e202718f132aaca95031dd64fac9c
Ksenia <torgaeva.ksenia@yandex.ru>
2018-01-07 10:47:35 +0300

    вьюшки для получения json

---
+++ imports.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import django
+django.setup()
+from sightseens.models import Visit, User, Location
+from django.utils import timezone
--- sightseens/views.py
+++ sightseens/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.utils.six import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
-from sightseens.models import User, Visit, Location
+from sightseens.models import User, Visit, Location, UserSerializer, VisitSerializer, LocationSerializer
 
 
 def index(request):
@@ -11,32 +14,42 @@ def index(request):
 
 
 def getEntity(request, entity, entity_id):
-    global u
+    global u, data
     if request.method == 'GET':
         if entity == 'users':
-            try:
-                u = User.objects.get(id=entity_id)
-            except Exception:
-                u = HttpResponse(status=404)
+            u = get_object_or_404(User, pk=entity_id)
+            serializer = UserSerializer(u)
+            data = getJsonData(serializer)
         elif entity == 'visits':
             u = Visit.objects.get(id=entity_id)
+            serializer = VisitSerializer(u)
+            data = getJsonData(serializer)
         elif entity == 'locations':
             u = Location.objects.get(id=entity_id)
+            serializer = LocationSerializer(u)
+            data = getJsonData(serializer)
         else:
             u = HttpResponse(status=404)
-    return HttpResponse(u)
+    return JsonResponse(data=data, safe=False)  # safe-False для сериализации не kv obj
 
 
-from django.template import loader
+def getJsonData(serializer):
+    dict_user = serializer.data
+    json = JSONRenderer().render(dict_user)
+    stream = BytesIO(json)
+    return JSONParser().parse(stream)
+
+
+from django.shortcuts import render
+
 
 def getPersonVisitList(request, entity_id):
-    person_visit_list=Visit.objects.filter(user=entity_id)
-    template = loader.get_template('sightseens/visit_list.html')
+    person_visit_list = Visit.objects.filter(user=entity_id)
     context = {
         'all_visits_list': person_visit_list,
         'user': User.objects.get(id=entity_id)
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'sightseens/visit_list.html', context)
 
 
 def getLocationAverageMark(request):
