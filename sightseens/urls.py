95998ea3dbe16083ec9ae249bcfe3847a948dd92
Ksenia <torgaeva.ksenia@yandex.ru>
2018-01-07 10:45:24 +0300

    До конца поправила url'ы

--- sightseens/urls.py
+++ sightseens/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:entity>/<int:entity_id>', views.getEntity, name='get_entity'),
     path('users/<int:entity_id>/visits', views.getPersonVisitList, name='get_visit_list_for_person'),
-    path(r'^location/(\d+)/avg', views.getLocationAverageMark, name='get_average_mark_for_location'),
-    # path(r'^(\d+)/(\d+)$', views.updateUser, name='update_user'),  # entity, id
-    path(r'^(\d+)/new$', views.createUser, name='new_user'),  # user
+    path('location/<int:entity_id>/avg', views.getLocationAverageMark, name='get_average_mark_for_location'),
+    path('<int:entity_id>/new', views.createUser, name='new_user'),
 ]
