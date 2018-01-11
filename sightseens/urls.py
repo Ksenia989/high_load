4de3040c0170cc9b273e5f648a85bdade39d405c
Ksenia <torgaeva.ksenia@yandex.ru>
2018-01-11 21:57:14 +0300

    поправила url

--- sightseens/urls.py
+++ sightseens/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:entity>/<int:entity_id>', views.getEntity, name='get_entity'),
     path('users/<int:entity_id>/visits', views.getPersonVisitList, name='get_visit_list_for_person'),
-    path('location/<int:entity_id>/avg', views.getLocationAverageMark, name='get_average_mark_for_location'),
+    path('locations/<int:entity_id>/avg', views.getLocationAverageMark, name='get_average_mark_for_location'),
     path('<int:entity_id>/new', views.createUser, name='new_user'),
 ]
